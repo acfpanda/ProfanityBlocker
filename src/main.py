5c9454afd7acd7b47ce37506f0ce5113c6325295
Alex Cabezas Fontanals <alex@panda.inc>
2021-09-23 17:07:23 +0200

    censors mostly everything okay

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from better_profanity import profanity
 
 # Function to get profanities from the file
 def getProfanities(fileName):
+    print(os.path.dirname(__file__) + fileName)
     fileObj = open(os.path.dirname(__file__) + fileName, "r")  # opens the file in read mode
     words = fileObj.read().replace(",", "\n")  # get list and prepare it for the splitlines
     profanities = words.splitlines()  # convert each line (profanity) into list element
@@ -14,12 +15,37 @@ def getProfanities(fileName):
         profanities[i] = profanities[i].strip()
     return profanities
 
-profanities = getProfanities("\\..\\bannedWords.txt")
-profanity.load_censor_words(profanities)
-print(profanity.censor('damn i have to watch a lot of fucking f u c k python tutorials these dick hole days'))
+# Censors profanities from a text string one by one, but ignores if profanity is inside word
+def removeProfanitiesV1(text):
+    for i, s in enumerate(profanities):
+        n = len(profanities[i])
+        replace = " "
+        replace += n * '*' + " "
+        text = text.replace(profanities[i], replace)
 
-# f = open("juice.html","w")
+    return text
 
-# 1 - Get HTML text file
+# Censors profanities using better_profanity functions, which work except when there is @, $, *, ", '
+def removeProfanitiesV2(text, profanities):
+    profanity.load_censor_words(profanities)
+    textCorrected = profanity.censor(text)
+    return textCorrected
 
-# 2 - Find inside <body>, normal text that has profanities
+profanities = getProfanities("\\..\\files\\bannedWords.txt")
+
+#Get text from file
+file2correctPath = "\\..\\files\\example2.html"
+file2correct = open(os.path.dirname(__file__) + file2correctPath, "r")
+text = file2correct.read()  # get the full html file
+file2correct.close()
+
+#Censor the profanities
+text = removeProfanitiesV2(text,profanities)
+
+#Save the changes in a new file
+f = open(os.path.dirname(__file__) + "\\..\\files\\example2corrected.html", "w")
+f.write(text)
+f.close()
+
+#Solution: modify better_profanity to adjust the few mistakes it makes
+
