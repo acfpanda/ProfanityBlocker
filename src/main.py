eb26a43989fadd8ddb2ad4f7fa7516adb9ed1cf7
Alex Cabezas Fontanals <alex@panda.inc>
2021-09-28 17:41:40 +0200

    Blocks all words and exludes most paths
    Only has problem with paths that contain " and . like web names. Doesn't seem to have problems with normal paths.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from better_profanity import profanity
 # Function to get profanities from the file
 def getProfanities(fileName):
     print(os.path.dirname(__file__) + fileName)
-    fileObj = open(os.path.dirname(__file__) + fileName, "r")  # opens the file in read mode
+    fileObj = open(os.path.dirname(__file__) + fileName, "r", encoding='utf-8')  # opens the file in read mode
     words = fileObj.read().replace(",", "\n")  # get list and prepare it for the splitlines
     profanities = words.splitlines()  # convert each line (profanity) into list element
     fileObj.close()
@@ -15,18 +15,8 @@ def getProfanities(fileName):
         profanities[i] = profanities[i].strip()
     return profanities
 
-# Censors profanities from a text string one by one, but ignores if profanity is inside word
-def removeProfanitiesV1(text):
-    for i, s in enumerate(profanities):
-        n = len(profanities[i])
-        replace = " "
-        replace += n * '*' + " "
-        text = text.replace(profanities[i], replace)
-
-    return text
-
 # Censors profanities using better_profanity functions, which work except when there is @, $, *, ", '
-def removeProfanitiesV2(text, profanities):
+def removeProfanities(text, profanities):
     profanity.load_censor_words(profanities)
     textCorrected = profanity.censor(text)
     return textCorrected
@@ -34,18 +24,16 @@ def removeProfanitiesV2(text, profanities):
 profanities = getProfanities("\\..\\files\\bannedWords.txt")
 
 #Get text from file
-file2correctPath = "\\..\\files\\example2.html"
-file2correct = open(os.path.dirname(__file__) + file2correctPath, "r")
+file2correctPath = "\\..\\files\\exemple1.html"
+file2correct = open(os.path.dirname(__file__) + file2correctPath, "r", encoding='utf-8')
 text = file2correct.read()  # get the full html file
 file2correct.close()
 
 #Censor the profanities
-text = removeProfanitiesV2(text,profanities)
+text = removeProfanities(text,profanities)
 
 #Save the changes in a new file
-f = open(os.path.dirname(__file__) + "\\..\\files\\example2corrected.html", "w")
+f = open(os.path.dirname(__file__) + "\\..\\files\\example1corrected.html", "w")
 f.write(text)
 f.close()
-
-#Solution: modify better_profanity to adjust the few mistakes it makes
 
